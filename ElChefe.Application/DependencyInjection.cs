ec4305b38b290600b6af6628f3e9852b776a2aa4
Greg Stewart <gregstewart7@gmail.com>
2022-06-28 18:26:59 +0100

    Add recipe service to dependency injection

--- ElChefe.Application/DependencyInjection.cs
+++ ElChefe.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using ElChefe.Application.Services.Authentication;
+using ElChefe.Application.Services.Recipes;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ElChefe.Application;
@@ -7,6 +8,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        return services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IRecipeService, RecipeService>();
+        return services;
     }
 }
